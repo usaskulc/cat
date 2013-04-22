071029de0d8d630d84b5aaccc40c4d34fdc000f9
abv641 <abv641@a2bd855a-d98e-4cf2-9477-e053e433ebb3>
2013-04-22 04:52:37 +0000

    fixed outcome select display

--- cat/src/ca/usask/gmcte/util/HTMLTools.java
+++ cat/src/ca/usask/gmcte/util/HTMLTools.java
@@ -106,6 +106,36 @@ public class HTMLTools
 				}
 				valueList.add(""+o.getId());
 				//the No match value should not receive a number
+				displayList.add( (i<1? "    ":i+". ")+ display);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return createSelect(name, valueList, displayList, ""+selectedValue,onchange);
+	}
+	public static String createSelect(String name, List<CourseOutcome> outcomes, Integer selectedValue, String onchange)
+	{
+		 
+		List<String> valueList = new ArrayList<String>();
+		valueList.add("-1");
+		List<String> displayList = new ArrayList<String>();
+		displayList.add("Please select an outcome to add");
+		int maxLength = 70;
+		try
+		{
+			
+			for(int i = 0; i < outcomes.size();  i++)
+			{
+				CourseOutcome o = outcomes.get(i);
+				String display = o.getName();
+				if (display.length() > maxLength+5)
+				{
+					display = display.substring(0,maxLength - 10) + " ... " + display.substring(display.length()-10); 
+				}
+				valueList.add(""+o.getId());
+				//the No match value should not receive a number
 				displayList.add( (i+1)+". "+ display);
 			}
 		}
