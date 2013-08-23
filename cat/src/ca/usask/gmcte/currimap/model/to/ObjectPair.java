504d08ce9bc8b4016104392d4df8aea7c76b7848
ede893 <ede893@a2bd855a-d98e-4cf2-9477-e053e433ebb3>
2013-08-23 19:18:21 +0000

    added GNU Licensing Blurb

--- cat/src/ca/usask/gmcte/currimap/model/to/ObjectPair.java
+++ cat/src/ca/usask/gmcte/currimap/model/to/ObjectPair.java
@@ -1,3 +1,24 @@
+/*****************************************************************************
+ * Copyright 2012, 2013 University of Saskatchewan
+ *
+ * This file is part of the Curriculum Alignment Tool (CAT).
+ *
+ * CAT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ * CAT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CAT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
+
+
 package ca.usask.gmcte.currimap.model.to;
 
 @SuppressWarnings("serial")
